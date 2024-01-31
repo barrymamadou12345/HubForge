e08f70fc39bb5d4af92f94f20650579d6f254251
Barry Mamadou <115747234+barrymamadou12345@users.noreply.github.com>
2024-01-31 22:27:21 +0000

    Wip : integration

--- frontend/src/components/composPageAccueil/PageAccueil.jsx
+++ frontend/src/components/composPageAccueil/PageAccueil.jsx
@@ -7,8 +7,11 @@ export const PageAccueil = () => {
       <div className="py-8 flex justify-center bg-gray-200">
         <div className="flex m-auto w-7/12 justify-around">
           <p className="text-2xl italic">Page d'accueil </p>
-          <Link className="text-2xl italic" to="templates">
-            Les Templates
+          <Link className="text-2xl italic" to="template1">
+            Les Template1
+          </Link>
+          <Link className="text-2xl italic" to="template2">
+            Les Template2
           </Link>
         </div>
       </div>
