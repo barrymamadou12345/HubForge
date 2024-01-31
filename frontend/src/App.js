612ec27926ba0270d731cda22016ea7ebd8e3f0f
kessoba <mariamabahdiallo1@gmail.com>
2024-01-31 11:00:05 +0000

    Wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 
+import React from 'react';
 import './App.css';
-import EnsembleTemp2 from './components/TemplateNum2/EnsembleTemp2';
+import { PageTemplate } from './components/PageTemplate';
+import { PageAccueil } from './components/composPageAccueil/PageAccueil';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <EnsembleTemp2/>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<PageAccueil />} />
+          <Route path="/accueil" element={<PageAccueil />} />
+          <Route path="/templates" element={<PageTemplate />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
