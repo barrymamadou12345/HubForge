703374876355956b0436609870e79059f9d53710
kessoba <mariamabahdiallo1@gmail.com>
2024-01-31 10:51:47 +0000

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import EnsembleTemp2 from './components/TemplateNum2/EnsembleTemp2';
 
 function App() {
   return (
     <div className="App">
-      <p className='text-red-500 text-center text-4xl font-bold mt-1'>Application</p>
+      <EnsembleTemp2/>
     </div>
   );
 }
---
+++ frontend/src/components/TemplateNum2/EnsembleTemp2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavTemp2 from './NavTemp2'
+import AccTemp2 from './AccTemp2'
+
+export default function EnsembleTemp2() {
+  return (
+    <div className='scrolled'>
+    <NavTemp2/>
+    <AccTemp2/>
+    </div>
+  )
+}
