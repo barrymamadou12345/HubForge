94890b60a04dc5e3f1ec1a6c6bf1ffef27e826c1
Cheikh-Ahmed-Tidiane-Gueye <cheikhahmedtidiane219@gmail.com>
2024-02-01 22:32:04 +0000

    integration du yemplate du site Ampire

--- frontend/src/components/PageTemplate.js
+++ frontend/src/components/PageTemplate.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import EnsembleTemp2 from './TemplateNum2/EnsembleTemp2'
-
+import Ampire from "../components/TemplateNum3/Ampire"
 export const PageTemplate = () => {
   return (
     <div>
-      <p className="text-center mt-[20%] text-4xl text-green-700 font-bold">
+      {/* <p className="text-center mt-[20%] text-4xl text-green-700 font-bold">
         Ckeikh tu REND ton deuxième template ICI dans PageTemplate
-      </p>
+      </p> */}
+      <Ampire/>
     </div>
   );
 }
