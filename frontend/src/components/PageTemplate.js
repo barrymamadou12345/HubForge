14828ef34048d1c3e2abe4d9434c55dd0c588dd7
Barry Mamadou <115747234+barrymamadou12345@users.noreply.github.com>
2024-02-02 00:53:16 +0000

    wip

--- frontend/src/components/PageTemplate.js
+++ frontend/src/components/PageTemplate.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import EnsembleTemp2 from './TemplateNum2/EnsembleTemp2'
 import Ampire from "../components/TemplateNum3/Ampire"
+export const PageTemplate = () => {
+  return (
+    <div>
+      {/* <p className="text-center mt-[20%] text-4xl text-green-700 font-bold">
+        Ckeikh tu REND ton deuxième template ICI dans PageTemplate
+      </p> */}
+      <Ampire/>
+    </div>
+  );
+}
 export const PageTemplate2 = () => {
   return (
     <div>
