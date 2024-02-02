ec326b1a984b6d15790da2ff82bda6e091d36af2
Barry Mamadou <115747234+barrymamadou12345@users.noreply.github.com>
2024-02-02 01:13:05 +0000

    wip

--- frontend/src/components/PageTemplate.js
+++ frontend/src/components/PageTemplate.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react";
 export const PageTemplate = () => {
   return (
     <div>
       <p className="text-center mt-[20%] text-4xl text-green-700 font-bold">
-        Ckeikh tu REND ton deuxième template ICI dans PageTemplate
+        autre template
       </p>
     </div>
   );
-}
+};
 export const PageTemplate2 = () => {
   return (
     <div>
       <p className="text-center mt-[20%] text-4xl text-red-700 font-bold">
-        Mariama tu REND ton deuxième template ICI Dans PageTemplate2
+        autre template
       </p>
     </div>
   );
