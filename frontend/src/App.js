fe0acebe94a092ecc33e71e26dcd416b56192ef2
Barry Mamadou <115747234+barrymamadou12345@users.noreply.github.com>
2024-02-02 00:16:18 +0000

    Revert "Ampire cheikh"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
-import {PageTemplate2 } from "./components/PageTemplate";
+import { PageTemplate, PageTemplate2 } from "./components/PageTemplate";
 import Alt from "./components/TemplateNum1/Alt";
 import { PageAccueil } from "./components/composPageAccueil/PageAccueil";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EnsembleTemp2 from "./components/TemplateNum2/EnsembleTemp2";
 import { AppProvider } from "./context/AppContext";
-import Ampire from "./components/TemplateNum3/Ampire";
 
 function App() {
   return (
@@ -18,7 +17,7 @@ function App() {
             <Route path="/accueil" element={<PageAccueil />} />
             <Route path="/template1" element={<Alt />} />
             <Route path="/template2" element={<EnsembleTemp2 />} />
-            <Route path="/template3" element={<Ampire />} />
+            <Route path="/template3" element={<PageTemplate />} />
             <Route path="/template4" element={<PageTemplate2 />} />
           </Routes>
         </BrowserRouter>
