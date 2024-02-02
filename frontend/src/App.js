fa1830edfc0ba1d4b0100dc77123206ddaa1c163
Barry Mamadou <115747234+barrymamadou12345@users.noreply.github.com>
2024-02-02 09:45:16 +0000

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import { PageAccueil } from "./components/composPageAccueil/PageAccueil";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EnsembleTemp2 from "./components/TemplateNum2/EnsembleTemp2";
 import { AppProvider } from "./context/AppContext";
-import Ampire from "./components/TemplateNum3/Ampire";
 //travail
 
 function App() {
